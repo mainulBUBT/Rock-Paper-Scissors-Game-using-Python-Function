c38ea146402a5ff40e87611ec5f4d3b084423e07
mainulBUBT <shopniilmihan@gmail.com>
2021-12-05 19:48:40 +0600

    Update RockPaperScissors.py

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -1,32 +1,24 @@
 def userInput(first_player, second_player):
+
     if first_player == second_player:
         print("Tie the game!")
+
     elif first_player == "rock":
         if second_player == "paper":
-            print("Player " + str(second_player) + " won the game!")
-    elif first_player == "paper":
-        if second_player == "rock":
+            print("Player-2 won the game!")
+        else:
             print("Player-1 won the game!")
 
     elif first_player == "scissors":
         if second_player == "rock":
             print("Player-2 won the game!")
-    elif first_player == "rock":
-        if second_player == "scissors":
-            print("Player-1 won the game!")
-
-    elif first_player == "scissors":
-        if second_player == "rock":
-            print("Player-2 won the game!")
-    elif first_player == "rock":
-        if second_player == "scissors":
+        else:
             print("Player-1 won the game!")
 
     elif first_player == "paper":
         if second_player == "scissors":
             print("Player-2 won the game!")
-    elif first_player == "scissors":
-        if second_player == "paper":
+        else:
             print("Player-1 won the game!")
     else:
         print("Enter valid rock/paper/scissors command")
